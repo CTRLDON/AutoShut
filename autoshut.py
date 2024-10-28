3b6eb74e8cf4be80e4ecd27a541a6b9501538f79
CTRLDON <a.m.elatawy@gmail.com>
2024-10-28 20:31:54 +0300

    commenting part of the main code

--- autoshut.py
+++ autoshut.py
@@ -10,47 +10,47 @@ import os
 
 class MainWindow():
     def __init__(self,root) -> None:
-        self.main_window = root
-        mainDir = os.path.dirname(os.path.abspath(__file__))
-        self.db = sqlite3.connect(os.path.join(mainDir,"times.db"))
-        self.cr = self.db.cursor()
-        dirname = os.path.dirname(__file__)
-        self.cr.execute("CREATE TABLE IF NOT EXISTS times(title text,hour text,min text,period text)")
-        qr = "SELECT * FROM times"
-        times = [time[0] for time in asset.db_get(self.cr , qr , "fetchall")]
+        self.main_window = root # creating the variable of the main window provided when creating an object
+        mainDir = os.path.dirname(os.path.abspath(__file__)) # the location of the current folder of the application
+        self.db = sqlite3.connect(os.path.join(mainDir,"times.db")) # connecting to the database in the current folder
+        self.cr = self.db.cursor() # creating the cursor
+        dirname = os.path.dirname(__file__) # getting the name of the folder
+        self.cr.execute("CREATE TABLE IF NOT EXISTS times(title text,hour text,min text,period text)") # creating the times table of not exists
+        qr = "SELECT * FROM times" # to find any time saved in the database
+        times = [time[0] for time in asset.db_get(self.cr , qr , "fetchall")] # creating a list with all the avaliable times
 
-        if len(times) != 0:
+        if len(times) != 0: # if there is any saved time start the shutdown process automatically
             self.shutdown()
         
-        json_check = os.path.exists(os.path.join(dirname,"config.json"))
-        if json_check == False:
+        json_check = os.path.exists(os.path.join(dirname,"config.json")) # checking if the json file exists
+        if json_check == False: # if it doesn't exist create a file
             asset.create_json()
         
-        check = asset.json_load("config.json")
+        check = asset.json_load("config.json") # loading the file
         
-        if check["error file"] == False:
+        if check["error file"] == False: # if the log file doesn't exist create a one
             asset.create_logging_folder()
-            check["error file"] = True
+            check["error file"] = True # make it true
             asset.json_dump(check , "config.json")
         
 
     def shutdown(self):
-        today = datetime.datetime.today()
-        qr = "SELECT * FROM times"
-        times = [time[0] for time in asset.db_get(self.cr , qr , "fetchall")]
-        current = datetime.datetime.now().strftime("%I:%M %p")
-        nearest_time = filteringSystem.ftime(times,current)
-        nearest_time = today.replace(hour=nearest_time.hour,minute=nearest_time.minute,second=0,microsecond=0)
-        print(nearest_time)
-        current = datetime.datetime.strptime(current,"%I:%M %p")
-        current = today.replace(hour=current.hour,minute=current.minute,second=datetime.datetime.now().second,microsecond=0)
-        if(nearest_time < current):
+        today = datetime.datetime.today() # getting the current day date
+        qr = "SELECT * FROM times" # a query for extracting all the existing time in the database
+        times = [time[0] for time in asset.db_get(self.cr , qr , "fetchall")] # creating a list to deal with
+        current = datetime.datetime.now().strftime("%I:%M %p") # formating the current time formart with a more readable format
+        nearest_time = filteringSystem.ftime(times,current) # applying an algorithm to return the nearest time to our current time
+        nearest_time = today.replace(hour=nearest_time.hour,minute=nearest_time.minute,second=0,microsecond=0) # replacing the time of today to the nearest time gotten from the algorithm
+        print(nearest_time) # checking the value
+        current = datetime.datetime.strptime(current,"%I:%M %p") 
+        current = today.replace(hour=current.hour,minute=current.minute,second=datetime.datetime.now().second,microsecond=0) # formatting the current time to compare with the future time
+        if(nearest_time < current): # if the current time is ahead of the next time then increase a day for the future date (like if the time is 3 am and you are at 11 pm you should a day difference to get the correct difference)
             nearest_time += datetime.timedelta(days=1)
-        sec = (nearest_time-current).total_seconds()
+        sec = (nearest_time-current).total_seconds() # getting the amount of seconds
         print(sec)
-        os.system(f"shutdown /s /t {int(sec)}")
+        os.system(f"shutdown /s /t {int(sec)}") # shutdown the pc given the amount of seconds
 
-    def abort(self):
+    def abort(self): # aborting the shutdown
         os.system(f"shutdown -a")
         
         
